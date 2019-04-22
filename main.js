80a8312730886eaa90b794659a403f61e7934683
Cengizhan <cengizhanpe@gmail.com>
2019-04-23 02:05:47 +0300

    add message send,receive,session

--- main.js
+++ main.js
@@ -1,20 +1,37 @@
 const express = require('express'),
       bodyParser = require('body-parser'),
       app = express(),
+      http = require('http').createServer(app);
+      io = require('socket.io')(http);
       signup = require('./router/signup'),
       signin = require('./router/signin'),
       session = require('express-session'),
+      cookie = require('cookie'),
+      cookieParser = require('cookie-parser'),
+      mongodb = require('mongodb').MongoClient,
+      connect = require('connect');
       port = process.env.PORT || 5000;
 
+      var onlineUsers = [];
+      var messages = [];
+ 
+// database connection
+const uri = encodeURI(process.env.MONGODB_URL);
+const option = { useNewUrlParser: true };
 
+
+/* Middlewares */
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(signup);
 app.use(signin);
 app.use(express.static(__dirname + '/public'));
 app.use(session({'secret': 'ChatApp',
-                  resave: true,
-                  saveUninitialized: true}));
+resave: true,
+saveUninitialized: true
+}));
+
+
 
 app.get('/signup', (req,res) =>{
   if(req.session.kullaniciId || req.session.sifre){
@@ -45,13 +62,99 @@ app.get('/index', (req,res) =>{
     res.redirect('/signin');
     return false;
   }
-  res.render(__dirname + '/views'+ '/chatroom.ejs', {err: undefined, kullaniciId: req.session.kullaniciId});
+  res.cookie('kullaniciId', req.session.kullaniciId);
+  res.render(__dirname + '/views'+ '/chatroom.ejs', {kullaniciId: req.session.kullaniciId});
 });
 
 app.get('*', (req,res)=>{
   res.send('404 NOT FOUND');
 })
 
-app.listen(port, () =>{
+
+/* SOCKET CODES */ 
+io.on('connection', function(socket){
+  var cookies = cookie.parse(socket.handshake.headers.cookie);
+  onlineUsers.push({socketId: socket.id, kullaniciId:cookies.kullaniciId });
+  
+  mongodb.connect(uri,option, (err,client)=>{
+    const db = client.db("mydb");
+
+    // wait for old messages come from database
+    var allMessage = () => new Promise((resolve,reject)=>{
+      db.collection("messages")
+      .find()
+      .toArray((err,result)=>{
+        if(err) reject(err);
+
+        resolve(result);
+      })
+    });
+    
+    allMessage()
+    .then(result=>{
+      result.forEach(element=>{
+        messages.push(element); // push all messages to array
+      })
+      io.emit("oldMessages", messages); //send to client
+    })
+    .catch((err)=>console.log(err));
+    client.close();
+  })
+
+
+  
+
+  io.emit("online users" ,onlineUsers);
+
+  socket.on('new message', (message)=>{
+    if(message.trim() != ""){
+      
+      onlineUsers.forEach(element=>{
+        if(element.socketId == socket.id){
+          mongodb.connect(uri, option, (err, client)=>{
+            const db = client.db("mydb");
+            db.collection("messages")
+            .insertOne({message: message, owner: element.kullaniciId}); // add to database
+            messages.push({message: message, owner: element.kullaniciId}) // add to array
+            io.emit("new message", message, element.kullaniciId);
+            client.close();
+          })
+        }
+      })
+
+
+    }
+  });
+
+  socket.on('disconnect', function(){
+    var geciciArray = [];
+    onlineUsers.forEach(element=>{
+      if(element.socketId == socket.id){}
+      else
+        geciciArray.push(element);
+    });
+      onlineUsers = [];
+    geciciArray.forEach(element=>{
+      onlineUsers.push(element);
+    });
+
+
+    mongodb.connect(uri, option, (err,client)=>{
+      if(err) console.log(err);
+      const db = client.db("mydb");
+      db.collection("sessions").deleteMany({ socketId : socket.id });
+      client.close();
+    });
+    io.emit("online users" ,onlineUsers);
+  });
+});
+
+/* ------------------------ */
+
+/* LISTEN PORT */
+http.listen(port, () =>{
   console.log("server is running on  "+ port + ". port");
 });
+
+
+
---
+++ public/scripts/message.js
@@ -0,0 +1,110 @@
+
+var socket = io();
+var uniqueUsers = [];
+socket.on('online users', function(users){
+    uniqueUsers = [];
+    users.forEach(element=>{
+        if(uniqueUsers.findIndex(x => x == element.kullaniciId) == -1){
+            uniqueUsers.push(element.kullaniciId);
+        }
+        uniqueUsers.forEach(element=>{
+            console.log(element);
+        })
+    })
+});
+
+document.getElementById("sendMessageBtn").addEventListener("click", function(){
+    var message = document.getElementById("sendMessageTxt");
+    var messageValue = message.value.trim();
+    if(messageValue != ""){
+
+        socket.emit("new message", messageValue);
+        message.value = "";
+    }
+});
+
+socket.on("oldMessages", (messages)=>{
+    let allMessages = document.getElementById("allMessages");
+    messages.forEach(element=>{
+        if(element.owner == urself){
+            let messageOutMain = document.createElement("div");
+            messageOutMain.className = "message-out-main";
+            let messageOut = document.createElement("div");
+            messageOut.className = "message-out";
+            let nameOut = document.createElement("div");
+            nameOut.className = "name-out";
+            let message = document.createElement("div");
+            message.className = "message";
+
+            nameOut.innerHTML = element.owner;
+            message.innerHTML = element.message;
+
+            messageOut.appendChild(nameOut);
+            messageOut.appendChild(message);
+            messageOutMain.appendChild(messageOut);
+            allMessages.appendChild(messageOutMain);
+        }
+        else{
+            let messageInMain = document.createElement("div");
+            messageInMain.className = "message-in-main";
+            let messageIn = document.createElement("div");
+            messageIn.className = "message-in";
+            let nameIn = document.createElement("div");
+            nameIn.className = "name-in";
+            let message = document.createElement("div");
+            message.className = "message";
+
+            nameIn.innerHTML = element.owner;
+            message.innerHTML = element.message;
+
+            messageIn.appendChild(nameIn);
+            messageIn.appendChild(message);
+            messageInMain.appendChild(messageIn);
+            allMessages.appendChild(messageInMain);
+        }
+    })
+    allMessages.scrollTop = allMessages.scrollHeight;
+})
+
+socket.on("new message", (messageContent,owner)=>{
+    if(owner == urself){
+        let messageOutMain = document.createElement("div");
+        messageOutMain.className = "message-out-main";
+        let messageOut = document.createElement("div");
+        messageOut.className = "message-out";
+        let nameOut = document.createElement("div");
+        nameOut.className = "name-out";
+        let message = document.createElement("div");
+        message.className = "message";
+
+        nameOut.innerHTML = owner;
+        message.innerHTML = messageContent;
+
+        messageOut.appendChild(nameOut);
+        messageOut.appendChild(message);
+        messageOutMain.appendChild(messageOut);
+        allMessages.appendChild(messageOutMain);
+    }
+    else{
+        let messageInMain = document.createElement("div");
+        messageInMain.className = "message-in-main";
+        let messageIn = document.createElement("div");
+        messageIn.className = "message-in";
+        let nameIn = document.createElement("div");
+        nameIn.className = "name-in";
+        let message = document.createElement("div");
+        message.className = "message";
+
+        nameIn.innerHTML = owner;
+        message.innerHTML = messageContent;
+
+        messageIn.appendChild(nameIn);
+        messageIn.appendChild(message);
+        messageInMain.appendChild(messageIn);
+        allMessages.appendChild(messageInMain);
+    }
+
+    if(allMessages.scrollHeight - allMessages.scrollTop <= 1000){
+        allMessages.scrollTop = allMessages.scrollHeight;
+    }
+})
